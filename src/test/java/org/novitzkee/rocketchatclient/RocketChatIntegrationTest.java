f85e715d3910776de2fb96463c3bdd93c91aeea2
Jarosław Nowicki <novitzkimain@gmail.com>
2026-04-04 21:12:13 +0200

    spin up test Rocket.Chat instance using testcontainers library

---
+++ src/test/java/org/novitzkee/rocketchatclient/RocketChatIntegrationTest.java
@@ -0,0 +1,80 @@
+package org.novitzkee.rocketchatclient;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.mongodb.MongoDBContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RocketChatIntegrationTest {
+
+    private static final DockerImageName MONGO_8 = DockerImageName.parse("mongo:8.0");
+
+    private static final DockerImageName ROCKET_CHAT_8 = DockerImageName.parse("rocket.chat:8.2.1");
+
+    private static final Network NETWORK = Network.newNetwork();
+
+    private static final String MONGO_NETWORK_ALIAS = "mongodb";
+
+    private static final int ROCKET_CHAT_PORT = 3000;
+
+    private static final MongoDBContainer MONGO_DB = new MongoDBContainer(MONGO_8)
+            .withNetwork(NETWORK)
+            .withNetworkAliases(MONGO_NETWORK_ALIAS)
+            .withReplicaSet()
+            .withCommand("--replSet rs --oplogSize 128 --bind_ip_all")
+            .withReuse(true);
+
+    private static final GenericContainer<?> ROCKET_CHAT = new GenericContainer<>(ROCKET_CHAT_8)
+            .withNetwork(NETWORK)
+            .withExposedPorts(ROCKET_CHAT_PORT)
+            .dependsOn(MONGO_DB)
+            .withEnv("MONGO_URL", String.format("mongodb://%s:27017/rocketchat", MONGO_NETWORK_ALIAS))
+            .withEnv("MONGO_OPLOG_URL", String.format("mongodb://%s:27017/local", MONGO_NETWORK_ALIAS))
+            .waitingFor(
+                    Wait.forHttp("/readyz")
+                            .forStatusCode(200)
+                            .withStartupTimeout(Duration.ofMinutes(1))
+            )
+            .withReuse(true);
+    // Reuse in this case only prevents killing the container after tests finish to inspect/debug.
+
+    @BeforeAll
+    static void spinUpRocketChat() {
+        MONGO_DB.start();
+        ROCKET_CHAT.start();
+    }
+
+    @Test
+    void shouldRetrieveAPIStatus() throws Exception {
+        // given
+        final HttpClient httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+
+        final URI requestUri = new URI(getRocketChatBaseUrl() + "/api/info");
+        final HttpRequest apiInfoRequest = HttpRequest.newBuilder(requestUri)
+                .GET()
+                .build();
+
+        // when
+        final HttpResponse<String> response = httpClient.send(apiInfoRequest, HttpResponse.BodyHandlers.ofString());
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(200);
+    }
+
+    private String getRocketChatBaseUrl() {
+        return String.format("http://%s:%d", ROCKET_CHAT.getHost(), ROCKET_CHAT.getMappedPort(ROCKET_CHAT_PORT));
+    }
+}
